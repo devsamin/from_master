c3568322de80672aa15dfaceb7926df1c84e7899
devsamin <abdullahalsamin79@gmail.com>
2025-07-09 06:10:09 -0700

    Reuseable from

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,15 @@ import Hookfrom from './components/Hookfrom/Hookfrom'
 import RefFrom from './components/RefFrom/RefFrom'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 import StateFullform from './components/StateFullform/StateFullform'
-
+import ReuseableFrom from './components/ReuseableFrom/ReuseableFrom'
 function App() {
+
+  const handelsginupData = (data) =>{
+    console.log(data)
+  }
+  const handelupdateData = (data) =>{
+    console.log(data)
+  }
 
   return (
     <>
@@ -14,7 +21,14 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFullform></StateFullform> */}
       {/* <RefFrom></RefFrom> */}
-      <Hookfrom></Hookfrom>
+      {/* <Hookfrom></Hookfrom> */}
+      <ReuseableFrom formtitle={'Sgin Up'} handelsubmit={handelsginupData}
+       > <div>
+        <h1>Sgin up</h1>
+        <p>Please now you should sgin up</p></div></ReuseableFrom>
+      <ReuseableFrom formtitle={'Profile Update'} submitbtn={'Update'} handelsubmit={handelupdateData}> <div>
+        <h1>Update Profile</h1>
+        <p>Alwase keep your profile</p></div></ReuseableFrom>
       
     </>
   )
---
+++ src/components/ReuseableFrom/ReuseableFrom.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ReuseableFrom = ({formtitle,handelsubmit,  submitbtn='Submit', children}) => {
+    const hendelevent = (e) =>{
+        e.preventDefault();
+        const data = {
+            name : e.target.name,
+            email : e.target.email
+        }
+        handelsubmit(data)
+    }
+
+    return (
+        <div>
+            <h2>{children}</h2>
+            <form onSubmit={hendelevent}>
+                <input type="text" name='name' />
+                <br />
+                <input type="email" name='email' />
+                <br />
+                <input type="submit" value={submitbtn} />
+            </form>
+        </div>
+    );
+};
+
+export default ReuseableFrom;
